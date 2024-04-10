fda3bcb2faf1938a0ab5a8ce2bf303fd5c8cc842
leehom <llh_01129@163.com>
2024-04-10 20:43:37 +0800

    添加自适应和修改rnoh包名，修改版本号发npm
    Signed-off-by: leehom <llh_01129@163.com>

--- harmony/rn_webview/src/main/ets/RNCWebViewTurboModule.ts
+++ harmony/rn_webview/src/main/ets/RNCWebViewTurboModule.ts
@@ -22,7 +22,7 @@
  * SOFTWARE.
  */
 
-import { TurboModule, TurboModuleContext } from 'rnoh/ts';
+import { TurboModule, TurboModuleContext } from '@rnoh/react-native-openharmony/ts';
 import { CallbackState, ShouldRequestUrl } from './ShouldRequestUrl';
 import Logger from './Logger'
 
