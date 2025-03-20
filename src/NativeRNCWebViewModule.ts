5b83e5838cb05885abed9615c13c63051c594e4b
“leehom6666” <liulihong6@h-partners.com>
2025-03-20 15:50:07 +0800

    feat: codegen-lib适配
    Signed-off-by: “leehom6666” <liulihong6@h-partners.com>

--- src/NativeRNCWebViewModule.ts
+++ src/NativeRNCWebViewModule.ts
@@ -1,3 +1,7 @@
+// Copyright (c) 2025 Huawei Device Co., Ltd. All rights reserved
+// Use of this source code is governed by a Apache-2.0 license that can be
+// found in the LICENSE file.
+
 import type { TurboModule } from 'react-native';
 import { TurboModuleRegistry } from 'react-native';
 import { Double } from 'react-native/Libraries/Types/CodegenTypes';
